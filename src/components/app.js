fb4eb6450f92db40ee47866a2585838e01dcd012
Alexei <alexander_mihalopoulos@comcast.com>
2018-01-22 13:19:37 -0500

    ecosystem setup

---
+++ src/components/app.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../styles/app.css';
+
+export default function app(){
+  return (
+    <div>
+      <h1>Test</h1>
+      <span>test desc</span>
+    </div>
+  )
+
+};
